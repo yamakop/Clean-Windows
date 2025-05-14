939516aa1aee9e4d47bb10558887e19187dd19b6
yamakop <armayama@protonmail.com>
2025-05-14 11:20:48 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,9 +1,9 @@
 @echo off
-:: Forcer l'execution en tant qu'administrateur
 openfiles >nul 2>&1 || (
     powershell -Command "Start-Process '%~f0' -Verb RunAs"; exit /b
 )
 
 set "LOGFILE=%TEMP%\Script_nettoyage.log"
 echo Démarrage du script >> "%LOGFILE%"
-echo %DATE%_______%TIME%_______%COMPUTERNAME%_______%USERNAME% >> "%LOGFILE%"
+echo %DATE%_______%TIME%_______%COMPUTERNAME%_______%ver% >> "%LOGFILE%"
+
